48e8a43f323edad8edb9292ac38f2cc11f842611
Eyas Valdez <eyasvaldez7@gmail.com>
2024-06-30 21:53:48 -0400

    feat: add example code and types

---
+++ src/lib/hello.ts
@@ -0,0 +1,1 @@
+export const hello_world = () => console.log("Hello, World")
---
+++ src/lib/main.ts
@@ -0,0 +1,4 @@
+import type { TMessage } from "../@types"
+
+export const important_message: TMessage = "hello"
+export const world = () => console.log(important_message)
