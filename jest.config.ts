23f06380950ba871577086b9fc144af986960d4f
Eyas Valdez <eyasvaldez7@gmail.com>
2024-07-01 00:00:18 -0400

    test: add tests and ci github action (#30)
    * test: add tests for our package
    
    * ci: add continuous integration github action

--- jest.config.ts
+++ jest.config.ts
@@ -27,9 +27,7 @@ const config: JestConfigWithTsJest = {
     // coverageDirectory: undefined,
 
     // An array of regexp pattern strings used to skip coverage collection
-    // coveragePathIgnorePatterns: [
-    //   "\\\\node_modules\\\\"
-    // ],
+    coveragePathIgnorePatterns: ["src/@types"],
 
     // Indicates which provider should be used to instrument code for coverage
     coverageProvider: "v8",
